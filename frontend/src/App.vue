85e626d13a3e5a7b5a30786806925faa1e838a99
Neil Van <nvq.alino@gmail.com>
2021-09-05 15:34:59 +0800

    setup vue frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import BaseLayout from "./components/BaseLayout.vue";
+</script>
+
+<template>
+  <BaseLayout />
+</template>
+
---
+++ frontend/src/components/BaseLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <p class="text-2xl font-bold">hi</p>
+</template>
+
+<style scoped>
+</style>
